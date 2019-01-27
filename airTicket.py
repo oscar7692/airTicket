02ea93c7bbfccfcbe40f8aca3be42d96c3bb9664
op <op@localhost.localdomain>
2019-01-27 16:07:35 -0600

    adding style formats, links, decorators and functions to airTicket.py

--- airTicket.py
+++ airTicket.py
@@ -1,12 +1,22 @@
 #!/bin/python3
 from flask import Flask
+from flask import render_template
 from flask import request
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/tickets')
+def generic():
+    return render_template('generic.html')
+
+
+@app.route('/params')
+def parameters():
     param = request.args.get('param1', 'parametro')
     return 'hello params is {}'.format(param)
 
