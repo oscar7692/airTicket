7d559e60d184169352ca203c32fbb709ff6adba4
oscar7692 <oscar.puld@gmail.com>
2019-02-03 15:41:12 -0600

    modified:   airTicket.py new file:   templates/about.html modified:   templates/base/base.html new file:   templates/emirates_ticket.html modified:   templates/index.html modified:   templates/macro_forms.html new file:   templates/qatar_ticket.html deleted:    templates/ticket.html

--- airTicket.py
+++ airTicket.py
@@ -4,23 +4,35 @@ from flask import render_template
 from flask import request
 import forms
 
+
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
     ticketform = forms.TicketForm()
-    comment = "macro test"
-    return render_template('index.html', comment = comment, form = ticketform)
-
-@app.route('/tickets')
-def generic():
-    return render_template('ticket.html')
+    title = "airTicket"
+    return render_template('index.html', title = title, form = ticketform)
 
 
-@app.route('/params')
-def parameters():
-    param = request.args.get('param1', 'parametro')
-    return 'hello params is {}'.format(param)
+@app.route('/emirates')
+def emirates():
+    title = "Emirates Airlines"
+    return render_template('emirates_ticket.html', title = title)
+
+
+@app.route('/qatar')
+def qatar():
+    title = "Qatar Airlines"
+    qatar_ticket = forms.TicketForm()
+    return render_template('qatar_ticket.html', title = title, form = qatar_ticket)
+
+
+# test function
+# @app.route('/params')
+# def parameters():
+#     param = request.args.get('param1', 'parametro')
+#     return 'hello params is {}'.format(param)
+
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5000)
