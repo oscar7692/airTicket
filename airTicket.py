7d4aaa748d89af1567d68d3f8755ca4942fc52a4
oscar7692 <oscar.puld@gmail.com>
2019-02-03 22:35:03 -0600

    modified:   __pycache__/forms.cpython-36.pyc modified:   airTicket.py modified:   forms.py modified:   templates/base/base.html modified:   templates/macro_forms.html modified:   templates/qatar_ticket.html

--- airTicket.py
+++ airTicket.py
@@ -7,24 +7,29 @@ import forms
 
 app = Flask(__name__, static_url_path='/static')
 
+
 @app.route('/')
 def index():
     ticketform = forms.TicketForm()
     title = "airTicket"
-    return render_template('index.html', title = title, form = ticketform)
+    return render_template('index.html', title=title, form=ticketform)
 
 
 @app.route('/emirates')
 def emirates():
     title = "Emirates Airlines"
-    return render_template('emirates_ticket.html', title = title)
+    return render_template('emirates_ticket.html', title=title)
 
 
-@app.route('/qatar')
+@app.route('/qatar', methods=['GET', 'POST'])
 def qatar():
     title = "Qatar Airlines"
-    qatar_ticket = forms.TicketForm()
-    return render_template('qatar_ticket.html', title = title, form = qatar_ticket)
+    qatar_ticket = forms.TicketForm(request.form)
+    if request.method == 'POST' and qatar_ticket.validate():
+        print(qatar_ticket.username.data)
+        print(qatar_ticket.email.data)
+        print(qatar_ticket.date.data)
+    return render_template('qatar_ticket.html', title=title, form=qatar_ticket)
 
 
 # test function
@@ -35,4 +40,4 @@ def qatar():
 
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 5000)
+    app.run(debug=True, port=5000)
--- forms.py
+++ forms.py
@@ -1,8 +1,13 @@
 from wtforms import Form
 from wtforms import StringField, TextField, DateField
 from wtforms.fields.html5 import EmailField
+from wtforms import validators
+
 
 class TicketForm(Form):
-    username = StringField('nombre completo')
-    email = EmailField('email')
-    date = DateField('fecha del viaje')
+    username = StringField('Full name',
+                           [validators.length(min=4, max=30,
+                                              message='field cannot sent'
+                                              ' empty!')])
+    email = EmailField('Email address')
+    date = DateField('Travel date')
