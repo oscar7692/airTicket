6f6237f73500447dae0f99b2b650359b42fe0b4d
oscar7692 <oscar.puld@gmail.com>
2019-03-12 23:26:21 -0600

    modified:   airTicket.py modified:   forms.py

--- airTicket.py
+++ airTicket.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python3.6
 from flask import Flask
+from flask_pymongo import PyMongo
 from flask import render_template
 from flask import request
 from flask_wtf import CSRFProtect
 import forms
+import json
 
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'my_secret_key'
 csrf_token = CSRFProtect(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/airticket"
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def index():
-    ticketform = forms.TicketForm()
-    title = "airTicket"
-    return render_template('index.html', title=title, form=ticketform)
+    data_qry = mongo.db.qatar_ticket
+    for data in data_qry.find():
+        data = data['qatar']
+        enddata = render_template('index.html', data=data)
+    return enddata
+    # ticketform = forms.TicketForm()
+    # title = "airTicket"
+    # return render_template('index.html', title=title, form=ticketform)
 
 
 @app.route('/emirates')
@@ -25,14 +34,18 @@ def emirates():
 
 
 @app.route('/qatar', methods=['GET', 'POST'])
-def qatar():
+def qatar_ticket():
     title = "Qatar Airlines"
     qatar_ticket = forms.TicketForm(request.form)
     if request.method == 'POST' and qatar_ticket.validate():
-        print(qatar_ticket.username.data)
-        print(qatar_ticket.email.data)
-        print(qatar_ticket.date.data)
-        print(qatar_ticket.carddetail.data)
+        qatar = mongo.db.qatar_ticket
+        data = request.form.to_dict()
+        qatar_ticket.insert({'qatar': data})
+        print('JSON', data)
+        # print(qatar_ticket.username.data)
+        # print(qatar_ticket.email.data)
+        # print(qatar_ticket.date.data)
+        # print(qatar_ticket.carddetail.data)
     else:
         print('form error')
     return render_template('qatar_ticket.html', title=title, form=qatar_ticket)
