ed0e210a5d12dea5229fcbe1e89596fb0bae44b7
oscar7692 <oscar.puld@gmail.com>
2019-03-27 22:14:10 -0600

    modified:   airTicket_app.py modified:   templates/qatar.html

--- airTicket_app.py
+++ airTicket_app.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3.6
 from flask import Flask
+from flask import make_response
 from flask_mail import Message
 from flask_pymongo import PyMongo
 from flask import render_template, redirect
 from flask import request
 from flask import url_for
 from flask_wtf import CSRFProtect
+import pdfkit
 # import forms
 
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'my_secret_key'
-app.config["MONGO_URI"] = "mongodb://localhost:27017/airTcketdb"
+app.config['MONGO_DBNAME'] = 'airTicketdb'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/airTicketdb"
 mongo = PyMongo(app)
 csrf_token = CSRFProtect(app)
 
@@ -23,6 +26,7 @@ def index():
     # back_to_home = redirect(url_for('index'),302)
     return render_template('index.html', title=title)
 
+
 @app.route('/ana', methods=["GET", "POST"])
 def ana():
     title = "Emirates Airlines"
@@ -32,6 +36,7 @@ def ana():
         emirates.insert({"ana": data})
         print("JSON", data)
     return render_template('ana.html', title=title)
+
 
 @app.route('/cathay', methods=["GET", "POST"])
 def cathay():
@@ -43,6 +48,7 @@ def cathay():
         print("JSON", data)
     return render_template('cathay.html', title=title)
 
+
 @app.route('/emirates', methods=["GET", "POST"])
 def emirates():
     title = "Emirates Airlines"
@@ -53,26 +59,29 @@ def emirates():
         print("JSON", data)
     return render_template('emirates.html', title=title)
 
+
 @app.route('/eva', methods=['GET', 'POST'])
 def eva():
     title = "Singapore Airlines"
     if request.method == 'POST':
         singapore = mongo.db.singapore
         data = request.form.to_dict()
-        singapore.insert({'eva':data})
+        singapore.insert({'eva': data})
         print('JSON', data)
     return render_template('eva.html', title=title)
+
 
 @app.route('/qatar', methods=['GET', 'POST'])
 def qatar():
     title = "Qatar Airlines"
-    # qatar_ticket = forms.TicketForm(request.form)
     if request.method == 'POST':
         qatar = mongo.db.qatar
         data = request.form.to_dict()
         qatar.insert({'qatar': data})
         print('JSON', data)
+    # pdf = pdf_creator('qatar.html')
     return render_template('qatar.html', title=title)
+
 
 @app.route('/singapore', methods=['GET', 'POST'])
 def singapore():
@@ -80,9 +89,18 @@ def singapore():
     if request.method == 'POST':
         singapore = mongo.db.singapore
         data = request.form.to_dict()
-        singapore.insert({'singapore':data})
+        singapore.insert({'singapore': data})
         print('JSON', data)
     return render_template('singapore.html', title=title)
+
+
+# def pdf_creator(self, data):
+#     rendered = render_template(data)
+#     pdf = pdfkit.from_string(rendered, False)
+#     response = make_response(pdf)
+#     response.headers['Content-Type'] = 'application/pdf'
+#     response.headers['Content-Disposition'] = 'attachment; filename=output.pdf'
+#     return response
 
 # test function
 # @app.route('/params')
--- pdf_creator.py
+++
@@ -1,7 +0,0 @@
-from flask import make_response
-import pdfkit
-
-pdf = pdfkit.from_string(rendered, False)
-response = make_response(pdf)
-response.headers['Content-Type'] = 'application/pdf'
-response.headers['Content-Disposition'] = 'attachment; filename=output.pdf'
